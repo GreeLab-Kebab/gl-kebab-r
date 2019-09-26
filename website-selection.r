666980457a8e3a3c504a4ba3a3f811c7a72523bd
Wesley Shann <sshann95@outlook.com>
2019-09-26 16:06:37 +0200

    Remove samples data and script to select samples

--- website-selection.r
+++
@@ -1,5 +0,0 @@
-df <- read.table("data/top-1m.csv", header = FALSE, sep=",")
-elements <- 30
-sample <- df[sample(nrow(df), 30, replace = FALSE, prob = NULL),]
-write.table(sample, "data/sample.csv", row.names = FALSE, col.names = FALSE, sep=",")
-
