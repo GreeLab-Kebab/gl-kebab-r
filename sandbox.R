f0629f6dcd04fae4cb6096cdd0ec04ba02fc4adf
Wesley Shann <sshann95@outlook.com>
2019-10-24 13:12:07 +0200

    Create sandbox script for miscelanous operations

---
+++ sandbox.R
@@ -0,0 +1,11 @@
+##
+# Green lab 2019 - Team Kebab
+# This R script is used
+##
+
+source('scripts/io.R')
+source('scripts/subject.R')
+source('scripts/preprocessing.R')
+source('scripts/test.R')
+
+data  <- kb_read_csv_formated()
